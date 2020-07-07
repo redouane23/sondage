5945c6771a86ecb2bcee31e1c1b8e12520907085
chafik <chafik.hnn@gmail.com>
2020-07-07 14:58:41 +0100

    Procfile for Heroku

--- config/database.php
+++ config/database.php
@@ -42,11 +42,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+//            'host' => env('DB_HOST', '127.0.0.1'),
+//            'database' => env('DB_DATABASE', 'forge'),
+//            'username' => env('DB_USERNAME', 'forge'),
+//            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -56,10 +56,10 @@ return [
             //'engine' => null,
             'engine' => 'InnoDB',
 
-//            'host' => 'us-cdbr-east-05.cleardb.net',
-//            'database' => 'heroku_e8bb5ef7b4d1c9a',
-//            'username' => 'b10960be76b9c0',
-//            'password' => 'c69bb8b3',
+            'host' => 'us-cdbr-east-02.cleardb.com',
+            'database' => 'heroku_31ea7b217db8327',
+            'username' => 'b11ba8754225df',
+            'password' => '44ed718a',
         ],
 
         'pgsql' => [
